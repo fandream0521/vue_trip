45d533be03c1b83341c1bc1de33f89e1056f71c2
wjp <2435878928@qq.com>
2025-10-26 11:05:11 +0800

    add vant tab bar

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,16 @@
 <script setup>
 import { RouterView } from 'vue-router';
-import TabBar from './components/tab-bar/TabBar.vue';
+import TabBarVant from './components/tab-bar/TabBarVant.vue';
 
 </script>
 
 <template>
-  <TabBar class="tab-bar"/>
-  
+  <TabBarVant class="tab-bar"/>
   <RouterView />
 </template>
 
 <style scoped>
 .tab-bar {
-  position: fixed;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  height: 50px;
+  --van-tabbar-item-icon-margin-bottom: 3px
 }
 </style>
---
+++ src/components/tab-bar/TabBarVant.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from "vue"
+import { useRouter } from "vue-router"
+import tabBarData from "@/assets/data/tab-bar"
+
+const router = useRouter();
+
+const curActive = ref("home");
+</script>
+
+<template>
+<van-tabbar v-model="curActive" route active-color="#ff9853">
+  <van-tabbar-item v-for="item in tabBarData" :name="item.key" :key="item.key" :to="item.path">
+    <span>{{ item.text }}</span>
+    <template #icon="props">
+      <img :src="props.active ? item.activeImage : item.image" />
+    </template>
+  </van-tabbar-item>
+</van-tabbar>
+</template>
+
+<style lang="less" scoped>
+:deep(.van-tabbar-item__icon) img {
+  width: 36px;
+  height: auto;
+}
+</style>
